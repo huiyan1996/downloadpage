4ce82fccaaab27a963b10851159e8eb5cdf4b7a0
kate <kate6450@gmail.com>
2022-04-01 00:57:37 +0800

    init download page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 px-0">
+        <img src="@/assets/img/banner.jpg" width="100%" alt="" srcset="">
+      </div>
+      <div class="bottomBg">
+        <div class="text-center">
+          <button type="button" class="btn btn-primary">点击下载安装</button>
+        </div>
+        <div class="mt-4">
+          <b-badge pill variant="primary">温馨提示</b-badge>
+          <p class="mt-3">下载完成打开应用，IOS系统会提示“未受信任的企业级开发者”。</p>
+          <p class="mt-3">解决方法如下:</p>
+          <ol>
+            <li>点击手机中的设置-点击通用，进入通用设置。</li>
+            <li>寻找通用设置中的设备管理，进入通用设置，查询刚刚提示未受信任的企业级开发者前缀，点击进入。</li>
+            <li>在弹出窗口点击信任操作，即完成信任应用</li>
+          </ol>
+          <p class="text-center mt-5">(返回桌面即可打开应用了)</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
